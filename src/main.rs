afe78f461889e74b76cb95e9544df62181733d23
Michael Smykowski <smycal@gmail.com>
2018-04-17 18:39:24 -0700

    put hangman game on a server - connet via websocket

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate names;
+extern crate ws;
 
-use std::io;
 use std::collections::HashSet;
 use names::{Generator, Name};
+use ws::{listen, Handler, Sender, Result, Message, CloseCode};
 
 #[derive(Debug)]
 struct Game {
@@ -13,30 +14,64 @@ struct Game {
 }
 
 impl Game {
+
     fn increment_miss(&mut self) {
         self.misses += 1;
     }
 
-    fn check_letter(&mut self, guess: &String) {
-        if self.word.contains(guess) {
-            for (i, letter) in self.word.iter().enumerate() {
-                if letter == guess {
-                    self.progress[i] = letter.clone();
-                }
+    fn update_progress(&mut self, guess: &String) {
+        for (i, letter) in self.word.iter().enumerate() {
+            if letter == guess {
+                self.progress[i] = letter.clone();
             }
-        } else {
-            self.increment_miss();
         }
     }
 
-    fn check_status(&self) -> Option<&str> {
-        if self.misses == 10 {
-            return Some("You lose");
-        } else if self.progress == self.word {
-            return Some("You win");
-        }
+}
 
-        None
+struct Server {
+    out: Sender,
+    game: Game,
+}
+
+impl Handler for Server {
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        let string_msg = msg.to_string();
+
+        let res = if self.game.guesses.insert(string_msg.clone()) {
+            check_letter(&mut self.game, &string_msg);
+            format!("word: {:?}, misses: {:?}", self.game.progress, self.game.misses)
+        } else {
+            format!("{} has already been guessed", msg)
+        };
+
+        let res = if self.game.misses == 10 {
+            self.game = start_game();
+            format!("You lose, try again...")
+        } else if self.game.progress == self.game.word {
+            self.game = start_game();
+            format!("You win, starting a new game!")
+        } else { res };
+
+        self.out.send(res)
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away   => println!("The client is leaving the site."),
+            _ => println!("The client encountered an error: {}", reason),
+        }
+    }
+
+}
+
+fn check_letter(game: &mut Game, guess: &String) {
+    if game.word.contains(guess) {
+        game.update_progress(guess);
+    } else {
+        game.increment_miss();
     }
 }
 
@@ -63,34 +98,5 @@ fn start_game() -> Game {
 }
 
 fn main() {
-    let mut game = start_game();
-
-    loop {
-        println!("Guess a letter");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess).unwrap();
-
-        let guess = guess.trim()
-                         .to_string();
-
-        if game.guesses.insert(guess.clone()) {
-            game.check_letter(&guess)
-        } else {
-            println!("{} has already been guessed", guess);
-        }
-
-        println!("{:?}", game.progress);
-        println!("You missed {} times", game.misses);
-
-        match game.check_status() {
-            Some(x) => {
-                println!("{}", x);
-                break;
-            },
-            None    => continue,
-        }
-    }
-
+    listen("127.0.0.1:3000", |out| Server { out: out, game: start_game() } ).unwrap();
 }
